13fab934c6203e1d100d91a73d9c8d401c49a90b
Kemal Sinanagic <kemal.sinanagic@student.artesis.be>
2012-10-17 09:57:00 +0200

    Very basic window layout with None/Start/Stop states

--- GuiKemi/appClientModule/WindowMain.java
+++ GuiKemi/appClientModule/WindowMain.java
@@ -5,12 +5,17 @@ import javax.swing.JLabel;
 import java.awt.BorderLayout;
 import javax.swing.JTextField;
 import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 
 public class WindowMain {
 
 	private JFrame frame;
 	private JTextField textField;
+	private enum State { None, Start, Stop};
+	private State state; // 0, 1, 2
+	private JButton btnNewButton;
 
 	/**
 	 * Launch the application.
@@ -39,7 +44,9 @@ public class WindowMain {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		state = State.Start;
 		frame = new JFrame();
+		frame.setResizable(false);
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
@@ -54,9 +61,29 @@ public class WindowMain {
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
-		JButton btnNewButton = new JButton("Start");
-		btnNewButton.setEnabled(false);
+		btnNewButton = new JButton("Start");
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				mouseButtonPressed(arg0);
+			}
+		});
 		btnNewButton.setBounds(335, 48, 89, 23);
 		frame.getContentPane().add(btnNewButton);
+	}
+	
+	private void mouseButtonPressed(MouseEvent ev) {
+		toggleState();
+		btnNewButton.setText(state.toString());
+	}
+	
+	private void toggleState() {
+		if(state == State.None) return;
+		if (state == State.Start) {
+			state = State.Stop; return; 
+		}
+		if (state == State.Stop) {
+			state = State.Start; return; 
+		}
 	}
 }
